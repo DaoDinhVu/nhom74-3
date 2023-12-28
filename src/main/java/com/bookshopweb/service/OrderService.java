a3858cf6d1be53e8c196f12d3f7370e2472a3ef2
21130521 <21130521@st.hcmuaf.edu.vn>
2023-12-28 19:24:30 +0700

    Commit 5 brnach-5
    Tạo lớp OrderService

---
+++ src/main/java/com/bookshopweb/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.bookshopweb.service;
+
+public class OrderService {
+}
--- src/main/java/com/bookshopweb/servlet/client/OrderServlet.java
+++ src/main/java/com/bookshopweb/servlet/client/OrderServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "OrderServlet", value = "/OrderServlet")
+@WebServlet(name = "OrderServlet", value = "/order")
 public class OrderServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
