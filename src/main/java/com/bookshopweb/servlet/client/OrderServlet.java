6d39ebfcf1f180f7b4070010a348cb98010891ff
21130521 <21130521@st.hcmuaf.edu.vn>
2023-12-28 19:22:54 +0700

    Commit 4 branch-5
    Tạo lại OrderServlet

--- src/main/java/com/bookshopweb/servlet/client/OrderServlet.java
+++ src/main/java/com/bookshopweb/servlet/client/OrderServlet.java
@@ -1,5 +1,19 @@
 package com.bookshopweb.servlet.client;
 
-public class OrderServlet {
-    
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "OrderServlet", value = "/OrderServlet")
+public class OrderServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
